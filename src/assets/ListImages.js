9284501c479a762334513e90f7691db8e8d8e67c
S2ShiroeS2 <tuquochung123@gmail.com>
2021-06-09 18:10:05 +0700

    [CODE] - component ProductCarousel: done.

--- src/assets/ListImages.js
+++ src/assets/ListImages.js
@@ -5,5 +5,15 @@ import BANNER_3 from "assets/images/banners/banner-03.png"
 import TREND_0 from "assets/images/trend/Trend-00.jpg"
 import TREND_1 from "assets/images/trend/Trend-01.jpg"
 import FEATURE from "assets/images/feature/feature.jpg"
+import PRODUCT_IM from "assets/images/product/women/product-item.jpg"
 
-export { BANNER_0, BANNER_1, BANNER_2, BANNER_3, TREND_0, TREND_1, FEATURE }
+export {
+    BANNER_0,
+    BANNER_1,
+    BANNER_2,
+    BANNER_3,
+    TREND_0,
+    TREND_1,
+    FEATURE,
+    PRODUCT_IM
+}
