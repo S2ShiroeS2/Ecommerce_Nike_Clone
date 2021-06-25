6c9a59ede0c61bc2aad73cb4984d8ee8171d2e5d
S2ShiroeS2 <tuquochung123@gmail.com>
2021-06-25 17:04:24 +0700

    [CODE] - Responsive ProductCarousel and ProductCarouselItem for all device mobile: done.

--- src/components/ProductCarousel/ProductCarousel.jsx
+++ src/components/ProductCarousel/ProductCarousel.jsx
@@ -1,16 +1,31 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Container, Row } from "react-bootstrap"
 import Slider from "react-slick"
 import { ProductCarouselItem } from "components/index"
 import "./Style-ProductCarousel.scss"
 
 export default function ProductCarousel(props) {
+    const [withDevice, setWidthDevice] = useState(window.innerWidth)
+
+    useEffect(() => {
+        window.addEventListener("resize", () => handleResize())
+    })
+
     const settings = {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
+        slidesToShow:
+            withDevice <= 576
+                ? 1
+                : withDevice > 576 && withDevice <= 768
+                ? 2
+                : 3,
         slidesToScroll: 1
+    }
+
+    const handleResize = () => {
+        setWidthDevice(window.innerWidth)
     }
     return (
         <div className="mt-5">
@@ -21,7 +36,7 @@ export default function ProductCarousel(props) {
                     </div>
                 </Row>
             </Container>
-            <Container className="pl-0 pr-0">
+            <Container className="product-carousel__slide-wrapper fix-p">
                 <Slider className="fix-row-15" {...settings}>
                     <ProductCarouselItem />
                     <ProductCarouselItem />
