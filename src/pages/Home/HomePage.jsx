6b115f78ebc3bbf70a8fcb4f5143a300495da8a5
S2ShiroeS2 <tuquochung123@gmail.com>
2021-07-05 17:30:26 +0700

    [CODE] - add trending for men page: done.

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -8,18 +8,21 @@ import {
     MerchMenu
 } from "components/index"
 import response from "data/ListProductHome.json"
+import resTrend from "data/TrendData.json"
 
 export default function HomePage() {
     const [dataProducts, setDataProducts] = useState()
+    const [dataTrend, setDataTrend] = useState()
 
     useEffect(() => {
         setDataProducts(response.listProduct)
-    }, [])
+        setDataTrend(resTrend?.home)
+    }, [dataProducts, dataTrend])
 
     return (
         <>
             <SlideBanner />
-            <Trend />
+            <Trend data={dataTrend} />
             <Feature />
             <ProductCarousel title="Sunny Day Must-Haves" data={dataProducts} />
             <MoreShoes />
--- src/pages/Men/MenPage.jsx
+++ src/pages/Men/MenPage.jsx
@@ -1,20 +1,24 @@
 import React, { useState, useEffect } from "react"
 import { NavigationChild, Banner } from "components/index"
 import data from "data/ListProductShoeMenPage.json"
-import { ProductCarousel } from "components/index"
+import resTrend from "data/TrendData.json"
+import { ProductCarousel, Trend } from "components/index"
 
 export default function MenPage() {
     const [listProduct, setListProduct] = useState()
+    const [dataTrend, setDataTrend] = useState()
 
     useEffect(() => {
         setListProduct(data?.shoes)
-    }, [listProduct])
+        setDataTrend(resTrend?.men)
+    }, [listProduct, dataTrend])
 
     return (
         <div className="mt-6">
             <NavigationChild />
             <Banner />
             <ProductCarousel title="Featured Footwear" data={listProduct} />
+            <Trend data={dataTrend} />
         </div>
     )
 }
